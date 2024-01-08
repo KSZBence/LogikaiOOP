2170818d75245f5d86e79721587199d2a914123b
KSZBence <kunszekelybence@gmail.com>
2024-01-08 14:50:23 +0100

    Lada

---
+++ src/logikaioop/Lada.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logikaioop;
+
+/**
+ *
+ * @author kunszekely.bence
+ */
+public class Lada {
+    private String allitas;
+    private String anyag;
+    private boolean kincs;
+
+    public Lada(String allitas, String anyag, boolean kincs) {
+        setAllitas(allitas);
+        setAnyag(anyag);
+        setKincs(kincs);
+    }
+
+    public String getAllitas() {
+        return allitas;
+    }
+
+    public void setAllitas(String allitas) {
+        this.allitas = allitas;
+    }
+
+    public String getAnyag() {
+        return anyag;
+    }
+
+    public void setAnyag(String anyag) {
+        this.anyag = anyag;
+    }
+
+    public boolean isKincs() {
+        return kincs;
+    }
+
+    public void setKincs(boolean kincs) {
+        this.kincs = kincs;
+    }
+
+    @Override
+    public String toString() {
+        return "Lada{" + "allitas=" + allitas + ", anyag=" + anyag + ", kincs=" + kincs + '}';
+    }
+    
+    
+}
