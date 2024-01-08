b8e7fdb37d75bcae51a92f21bd8c2b62f00d2698
Christuss <szendefi.krisztofer@gmail.com>
2024-01-08 13:28:25 +0100

    main kiirasok

---
+++ src/logikaioop/LogikaiOOP.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logikaioop;
+
+/**
+ *
+ * @author szendefi.k.daniel
+ */
+public class LogikaiOOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Leírás");
+        System.out.println("Az 1. állítás!");
+        System.out.println("Az 2. állítás!");
+        System.out.println("Az 3. állítás!");
+        System.out.println("Melyik:");
+        System.out.println("A válasz jó | rossz");
+        
+    }
+    
+}
