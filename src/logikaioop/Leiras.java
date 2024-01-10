e40c6b5e9c3008daca427473e5bca0b80f5fb239
KSZBence <kunszekelybence@gmail.com>
2024-01-10 13:12:44 +0100

    eredmeny jelzes

--- src/logikaioop/Leiras.java
+++ src/logikaioop/Leiras.java
@@ -8,11 +8,15 @@ import java.util.Scanner;
 public class Leiras {
 
     public static Scanner sc = new Scanner(System.in);
+    Lada[] ladak = new Lada[3];
     
     public Leiras() {
         Lada l1 = new Lada("Az 1. állitas", "anyag", false);
         Lada l2 = new Lada("Az 2. állitas", "anyag", false);
         Lada l3 = new Lada("Az 3. állitas", "anyag", true);
+        ladak[0] = l1;
+        ladak[1] = l2;
+        ladak[2] = l3;
         
         kiir("Leírás");
         kiir(l1.toString());
@@ -20,18 +24,17 @@ public class Leiras {
         kiir(l3.toString());
         
         System.out.print("Melyik:");
-        int s = sc.nextInt();
+        int valasztott = sc.nextInt();
         kiir("");
-        kiir(new Leiras().eredmenyjelzes(true));
-        kiir(new Leiras().eredmenyjelzes(false));
+        kiir(new Leiras().eredmenyjelzes(valasztott));
     }
     
     public void kiir(String szoveg){
         System.out.println(szoveg);
     }
     
-    public String eredmenyjelzes(boolean igaz) {
-        if (igaz) {
+    public String eredmenyjelzes(int valasztott) {
+        if (ladak[valasztott-1].isKincs()) {
             return "A válasz jó!";
         } else {
             return "A válasz rossz!";
