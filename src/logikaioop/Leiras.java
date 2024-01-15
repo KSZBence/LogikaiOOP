77bee792b26fc64acb57259829ad9d4dba11ad9b
KSZBence <kunszekelybence@gmail.com>
2024-01-15 14:27:38 +0100

    view kialakitasa

--- src/logikaioop/Leiras.java
+++ src/logikaioop/Leiras.java
@@ -5,10 +5,11 @@
 package logikaioop;
 import model.Lada;
 import java.util.Scanner;
+import view.View;
 
 public class Leiras {
 
-    public static Scanner sc = new Scanner(System.in);
+    
     Lada[] ladak = new Lada[3];
     
     public Leiras() {
@@ -19,17 +20,7 @@ public class Leiras {
         ladak[1] = l2;
         ladak[2] = l3;
 
-        kiir("Leírás: minden láda 1-1 állítást tartalmaz, csak az egyik igaz.");
-        kiir(l1.toString());
-        kiir(l2.toString());
-        kiir(l3.toString());
         
-
-        System.out.print("Melyikben van a kincs? ");
-
-        int valasztott = sc.nextInt();
-        kiir("");
-        kiir(eredmenyjelzes(valasztott));
     }
 
 }
--- src/view/View.java
+++ src/view/View.java
@@ -3,12 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package view;
+import java.util.Scanner;
+import model.Lada;
 
 /**
  *
  * @author kunszekely.bence
  */
 public class View {
+    public static Scanner sc = new Scanner(System.in);    
+    Lada[] ladak;
+    
+    public View(Lada[] ladak) {
+        this.ladak = ladak;
+        kiir("Leírás: minden láda 1-1 állítást tartalmaz, csak az egyik igaz.");
+        for (Lada lada : ladak) {
+            kiir(lada.toString());
+        }
+
+        System.out.print("Melyikben van a kincs? ");
+        int valasztott = sc.nextInt();
+        kiir("");
+        kiir(eredmenyjelzes(valasztott));
+    }
+        
+        
     public void kiir(String szoveg){
         System.out.println(szoveg);
     }
